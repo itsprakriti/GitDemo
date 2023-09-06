9b10d9960952b07bfb44671ea0eb01a87b7f252e
prakriti <prakriti.2015@gmail.com>
2023-09-06 17:26:23 +0530

    first commit

---
+++ src/Program02_Comment.java
@@ -0,0 +1,16 @@
+
+public class Program02_Comment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is single line comment
+		System.out.println("Single Line Comment");
+		
+		/*
+		 This is Multi-line comment
+	    */
+		System.out.println("Multi Line Comment");
+	}
+
+}
---
+++ src/Program03_Variable.java
@@ -0,0 +1,11 @@
+
+public class Program03_Variable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str = "Prakriti";
+		System.out.println(str);
+	}
+
+}
